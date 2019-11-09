ae01d02d2d32458ad5c49fbae730037a450e5665
Camila Rori <camila.a.rori@gmail.com>
2019-11-09 03:34:31 -0300

    TP3

---
+++ TP3/Rori.Camila.2C.TP3/Clases Abstractas/Persona.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excepciones;
+
+namespace Clases_Abstractas
+{
+    public abstract class Persona
+    {
+        #region Campos
+        private string nombre;
+        private string apellido;
+        private int dni;
+        private ENacionalidad nacionalidad;
+        
+        #endregion
+
+        #region Propiedades
+        /// <summary>
+        /// Nacionalidad de la persona
+        /// </summary>
+        public ENacionalidad Nacionalidad
+        {
+            get { return this.nacionalidad; }
+            set { this.nacionalidad = value; }
+        }
+        /// <summary>
+        /// DNI, lo valida antes de ingresar
+        /// </summary>
+        public int DNI
+        {
+            get { return this.dni; }
+            set { this.dni = ValidarDni(this.nacionalidad, value); }
+        }
+        /// <summary>
+        /// Nombre de la persona. Lo valida antes de ingresar.
+        /// </summary>
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set
+            {
+                if (ValidarNombreApellido(value) != null)
+                    this.nombre = value;
+            }
+        }
+        public string Apellido
+        {
+            get { return this.apellido; }
+            set
+            {
+                if (ValidarNombreApellido(value) != null)
+                    this.apellido = value;
+            }
+        }
+        /// <summary>
+        /// DNI, lo valida antes de ingresar
+        /// </summary>
+        public string StringToDNI                  
+        {
+            set { this.dni = ValidarDni(this.nacionalidad,value); }
+        }
+        #endregion
+
+        #region Metodos
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Persona()
+        {
+
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="nacionalidad"></param>
+        public Persona(string nombre, string apellido, ENacionalidad nacionalidad)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Nacionalidad = nacionalidad;
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        public Persona(string nombre, string apellido, int dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            this.DNI = dni;            
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        public Persona(string nombre, string apellido, string dni, ENacionalidad nacionalidad) : this(nombre, apellido, nacionalidad)
+        {
+            this.StringToDNI = dni;
+        }
+        /// <summary>
+        /// Retornará los datos de la Persona
+        /// </summary>
+        /// <returns>String</returns>
+        public override string ToString()
+        {
+            StringBuilder mensaje = new StringBuilder("");
+            mensaje.AppendFormat("NOMBRE COMPLETO: {0}, {1}\n", this.Apellido,this.Nombre);
+            mensaje.AppendFormat("NACIONALIDAD: {0}\n", this.Nacionalidad);
+            //mensaje.AppendFormat("DNI: {0}\n", this.DNI);     > no incluyo DNI porque el archivo de ejemplo no lo incluye en profesor ni alumno, y reutilizo código
+            return mensaje.ToString();
+        }
+        /// <summary>
+        /// Valida que el DNI sea correcto según la nacionalidad y el tamaño
+        /// </summary>
+        /// <param name="nacionalidad"></param>
+        /// <param name="dato"></param>
+        /// <returns>DNI validado</returns>
+        private static int ValidarDni(ENacionalidad nacionalidad, int dato) 
+        {
+            if(nacionalidad==ENacionalidad.Argentino && dato> 89999999)
+            {
+                throw new NacionalidadInvalidaException("La nacionalidad no se condice con el número de DNI");
+            }
+            if (nacionalidad == ENacionalidad.Extranjero && dato < 90000000)
+            {
+                throw new NacionalidadInvalidaException("La nacionalidad no se condice con el número de DNI");
+            }
+            if (dato<1 || dato > 99999999)
+            {
+                throw new DniInvalidoException("Error de formato: más caracteres de los permitidos");   //error de formato
+            }
+            return dato;
+            
+        }
+        /// <summary>
+        /// Valida que el DNI solo contenga números y sea correcto según la nacionalidad y el tamaño
+        /// </summary>
+        /// <param name="nacionalidad"></param>
+        /// <param name="dato"></param>
+        /// <returns>DNI validado</returns>
+        private static int ValidarDni(ENacionalidad nacionalidad, string dato) 
+        {
+            int datoParse;        
+            if (!Int32.TryParse(dato, out datoParse))
+                throw new DniInvalidoException("Error de formato: contiene caracteres que no son dígitos");    
+            return ValidarDni(nacionalidad,datoParse);                            
+        }
+        /// <summary>
+        /// Valida que los nombres solo contengan caracteres validos
+        /// </summary>
+        /// <param name="dato"></param>
+        /// <returns>Nombre validado</returns>
+        private static string ValidarNombreApellido(string dato)
+        {
+            foreach(char letra in dato)
+            {
+                if (!(char.IsLetter(letra) || char.IsWhiteSpace(letra)))
+                    return null;
+            }
+            return dato;
+        }
+
+        #endregion
+
+        public enum ENacionalidad { Argentino, Extranjero }
+
+    }
+}
---
+++ TP3/Rori.Camila.2C.TP3/Clases Instanciables/Alumno.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases_Abstractas;
+using EClases = Clases_Instanciables.Universidad.EClases;
+
+namespace Clases_Instanciables
+{
+    public sealed class Alumno : Universitario
+    {
+        private EClases claseQueToma;
+        private EEstadoCuenta estadoCuenta;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Alumno()
+        {
+
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        /// <param name="claseQueToma"></param>
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, EClases claseQueToma)
+            : base(id,nombre,apellido,dni,nacionalidad)
+        {
+            this.claseQueToma = claseQueToma;
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="nombre"></param>
+        /// <param name="apellido"></param>
+        /// <param name="dni"></param>
+        /// <param name="nacionalidad"></param>
+        /// <param name="claseQueToma"></param>
+        /// <param name="estadoCuenta"></param>
+        public Alumno(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad, EClases claseQueToma, EEstadoCuenta estadoCuenta)
+            :this(id, nombre, apellido, dni, nacionalidad,claseQueToma)
+        {
+            this.estadoCuenta = estadoCuenta;
+        }
+
+        /// <summary>
+        /// Retorna los datos del alumno
+        /// </summary>
+        /// <returns>String</returns>
+        protected override string MostrarDatos()
+        {
+            StringBuilder mensaje = new StringBuilder(base.MostrarDatos());
+            mensaje.Append("\nESTADO DE CUENTA: ");
+            mensaje.Append((this.estadoCuenta == EEstadoCuenta.AlDia) ? "Cuota al día" : this.estadoCuenta.ToString());
+            mensaje.AppendLine(this.ParticiparEnClase());
+            mensaje.AppendLine();
+            return mensaje.ToString();
+        }
+        /// <summary>
+        /// Retornará la cadena "TOMA CLASE DE " junto al nombre de la clase que toma
+        /// </summary>
+        /// <returns>String</returns>
+        protected override string ParticiparEnClase()
+        {
+            return string.Format("\nTOMA CLASE DE {0}", this.claseQueToma);
+        }
+
+        /// <summary>
+        /// Retornará la cadena "TOMA CLASE DE " junto al nombre de la clase que toma
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return this.MostrarDatos();
+        }
+
+        /// <summary>
+        /// Un Alumno será igual a un EClase si toma esa clase y su estado de cuenta no es Deudor.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="clase"></param>
+        /// <returns></returns>
+        public static bool operator ==(Alumno a, EClases clase)
+        {
+            return (a.claseQueToma == clase && a.estadoCuenta != EEstadoCuenta.Deudor);
+        }
+
+        /// <summary>
+        /// Un Alumno será distinto a un EClase sólo si no toma esa clase.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="clase"></param>
+        /// <returns></returns>
+        public static bool operator !=(Alumno a, EClases clase)
+        {
+            return a.claseQueToma != clase;         //no reutilizo código porque si lo hiciera, si toma la clase y su estado de deuda es Deudor, a y clase son distintos, la consigna solo pide analizar si toma la clase
+        }
+
+        public enum EEstadoCuenta { AlDia, Deudor, Becado}
+        
+    }
+}
